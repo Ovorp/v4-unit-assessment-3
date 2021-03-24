ca6a880d6c2f1b27ea79eee29b526e1ec7e71b25
Nathaniel Shirts <nshirts7@gmail.com>
2021-03-23 19:32:58 -0600

    set up project

---
+++ booklist/src/App.js
@@ -0,0 +1,14 @@
+import './css/App.css';
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div>Hello World</div>;
+  }
+}
