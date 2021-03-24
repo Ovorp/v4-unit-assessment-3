6459ef81943def06e3369deb8a4233a99377c1ea
Nathaniel Shirts <nshirts7@gmail.com>
2021-03-23 21:55:21 -0600

    added a funny message on the shelf

--- booklist/src/components/Shelf.js
+++ booklist/src/components/Shelf.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 export default class Shelf extends Component {
   render() {
+    console.log(this.props.shelf.length);
     return (
       <div className="shelf">
         <h2>Your Shelf</h2>
+        {this.props.shelf.length === 0 ? (
+          <p>You should read more!!</p>
+        ) : (
+          <p>I'm glad you are reading :)</p>
+        )}
         <button onClick={this.props.clearShelf}>Clear</button>
         <div className="shelf-list">
           <ul>
